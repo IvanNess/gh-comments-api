1718e41e5329163da5d57c9cb02ebb0a86e38668
IvanNess <chai@bk.ru>
2021-06-05 23:29:44 +0200

    trying to get api/notification error 9

--- pages/api/notification.js
+++ pages/api/notification.js
@@ -13,17 +13,18 @@ import NextCors from 'nextjs-cors';
 // )
 
 export default async(req, res) => {
+
+    await NextCors(req, res, {
+        // Options
+        // methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+        origin: "http://clubelo.com",
+        // optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+     });
+
     console.log('notification req', req)
 
     try {
         // await cors(req, res)
-
-        await NextCors(req, res, {
-            // Options
-            // methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-            origin: "http://clubelo.com",
-            // optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-         });
 
         console.log('after cors', )
 

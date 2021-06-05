2bc957ba48f408ab2d974d3749fe093fb2d84daa
IvanNess <chai@bk.ru>
2021-06-05 20:34:03 +0200

    trying to get api/notification error

---
+++ pages/api/notification.js
@@ -0,0 +1,37 @@
+import NextCors from 'nextjs-cors';
+import initMiddleware from '../../init-middleware';
+const mongoose = require('mongoose')
+const PackageSchema = require('../../mongo-models/package-model')
+
+const cors = initMiddleware(
+    // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+    Cors({
+        // origin: process.env.ORIGIN,
+        // credentials: true
+        //   origin: false
+        origin: "http://clubelo.com",
+        credentials: true,
+        methods: ["GET"]
+    })
+)
+
+export default async(req, res) => {
+    console.log('notification req', req)
+
+    try {
+        await cors(req, res)
+
+        console.log('after cors', )
+
+        const {
+            merchantId, posId, sessionId, amount, originAmount, currency, orderId, methodId, statement, sign
+        } = req.body
+
+        return res.status(200).json('ok') 
+
+    } catch (error) {
+        console.log('error', error)
+        return res.status(500).json({message: error.message})
+    }
+
+}
